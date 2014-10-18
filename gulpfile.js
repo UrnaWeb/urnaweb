bca959019adbf59cde5f5509728ae6db4c75fa5f
Kaue Miyauti Machado <kaumac@gmail.com>
2014-10-18 00:12:53 -0300

    wip

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+var gulp        = require('gulp'),
+    usemin      = require('gulp-usemin'),
+    rev         = require('gulp-rev'),
+    minifyHtml  = require('gulp-minify-html'),
+    gutil       = require('gulp-util'),
+    sass        = require('gulp-sass'),
+    csso        = require('gulp-csso'),
+    uglify      = require('gulp-uglify'),
+    concat      = require('gulp-concat'),
+    livereload  = require('gulp-livereload'),
+    express     = require('express'),
+    app         = express(),
+    path        = require('path'),
+    neat        = require('node-neat').includePaths,
+    ngAnnotate  = require('gulp-ng-annotate'),
+    connect     = require('gulp-connect'),
+    changed     = require('gulp-changed'),
+    imagemin    = require('gulp-imagemin'),
+    jsonminify  = require('gulp-jsonminify');
+
+gulp.task('build-sass', function() {
+  return gulp.src('stylesheet/**/*.scss')
+    .pipe(
+      sass( {
+        includePaths: ['stylesheet'].concat(neat),
+        errLogToConsole: true
+      }))
+    .pipe( gulp.dest('tmp') )
+});
+
+// =====================================================================================
+//  Development related tasks
+// =====================================================================================
+gulp.task('connect', function() {
+  connect.server({
+    livereload: true
+  });
+});
+gulp.task('reload-css', function () {
+  gulp.src('./tmp/*.css')
+    .pipe(connect.reload());
+});
+gulp.task('reload-html', function () {
+  gulp.src('./**/*.html')
+    .pipe(connect.reload());
+});
+gulp.task('reload-js', function () {
+  gulp.src('./javascript/**/*.js')
+    .pipe(connect.reload());
+});
+gulp.task('watch', function () {
+    gulp.watch(['./stylesheet/**/*.{less,scss}'],['build-sass']);
+    gulp.watch(['tmp/*.css'],['reload-css']);
+    gulp.watch('javascript/**/*.js',['reload-js']);
+    gulp.watch(['index.html','templates/**/*.html'],['reload-html']);
+});
+
+// =====================================================================================
+//  Production related tasks
+// =====================================================================================
+gulp.task('image-compress', function() {
+  return gulp.src(['./images/*','./images/**/*'])
+      .pipe(changed('./dist/images'))
+      .pipe(imagemin())
+      .pipe(gulp.dest('./dist/images'))
+});
+gulp.task('build-html', function() {
+  gulp.src(['./templates/*.html','./templates/**/*.html','./templates/**/**/*.html'])
+    .pipe(minifyHtml({empty: true}))
+    .pipe(gulp.dest('./dist/templates'))
+});
+gulp.task('build-json', function () {
+    gulp.src(['languages/*.json'])
+        .pipe(jsonminify())
+        .pipe(gulp.dest('./dist/languages'));
+});
+gulp.task('usemin', function() {
+  gulp.src('index.html')
+    .pipe(usemin({
+      html: [minifyHtml({empty: true})],
+      css: [csso(), rev()],
+      js: [ngAnnotate(), uglify(), rev()]
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+// =====================================================================================
+//  Main tasks registration
+// =====================================================================================
+gulp.task('default', ['connect','watch']);
+gulp.task('build', ['build-sass','image-compress','build-json','build-html','usemin']);
