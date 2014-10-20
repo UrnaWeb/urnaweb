c09570a64410b7d5dedb02a3d1659f00e33471ae
Kaue Miyauti Machado <kaumac@gmail.com>
2014-10-20 00:34:48 -0200

    wip auth

--- javascript/vote_controller.js
+++ javascript/vote_controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('UrnaWeb').controller('VoteController', function($scope, $state, Vote) {
+angular.module('UrnaWeb').controller('VoteController', function($scope, $state, Vote, User, Auth) {
   $scope.vote = {};
   $scope.confirm_vote = function(party) {
     $scope.show_confirmation = true;
@@ -9,7 +9,20 @@ angular.module('UrnaWeb').controller('VoteController', function($scope, $state, 
   $scope.set_vote = Vote.set;
 
   $scope.submit_vote = function() {
-    $scope.set_vote = Vote.set({});
+    User.get_auth_payload().then(function(user) {
+      if(user === null) {
+        $scope.login_required = true;
+        $scope.$on("$firebaseSimpleLogin:login", function(e, user) {
+          console.log("User " + user.uid + " successfully logged in!");
+        });
+      } else {
+        $scope.set_vote = Vote.set({});
+      }
+    });
+  }
+
+  $scope.login = function() {
+    Auth.login();
   }
 
   $scope.cancel_vote = function() {
