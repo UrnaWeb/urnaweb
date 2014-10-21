aae37fe8de503f347295bcd036300a0df43d71a2
Kaue Miyauti Machado <kaumac@gmail.com>
2014-10-21 10:37:18 -0200

    wip

--- javascript/results_controller.js
+++ javascript/results_controller.js
@@ -1,58 +1,13 @@
 'use strict';
 
-angular.module('UrnaWeb').controller('ResultsController', function($scope, $state, Vote, User, Auth, totalVotes) {
-  $scope.vote = {
-    is_private: false
-  };
-  $scope.confirm_vote = function(party) {
-    $scope.show_confirmation = true;
-    $scope.vote.party = party;
-  }
-  $scope.set_vote = Vote.set;
-
-  $scope.submit_vote = function() {
-    User.get_auth_payload().then(function(user) {
-      if(user === null) {
-        $scope.login_required = true;
-      } else {
-        $scope.set_vote = Vote.set({
-          party: $scope.vote.party,
-          is_private: $scope.vote.is_private
-        }, function() {
-          $scope.vote_confirmed = true;
-          var audio = new Audio('urna.mp3');
-          audio.play();
-        });
-      }
-    });
-  }
-
-  $scope.login = function() {
-    Auth.login(function() {
-      $scope.set_vote = Vote.set({
-        party: $scope.vote.party,
-        is_private: $scope.vote.is_private
-      }, function() {
-        $scope.vote_confirmed = true;
-        var audio = new Audio('urna.mp3');
-        audio.play();
-      });
-    });
-  }
-
+angular.module('UrnaWeb').controller('ResultsController', function($scope, $filter, $state, User, Auth, totalVotes) {
   $scope.logout = function() {
     Auth.logout();
   }
 
-  $scope.cancel_vote = function() {
-    $scope.vote = {is_private: false};
-    $scope.show_confirmation = false;
-  }
+  $scope.total_votes = totalVotes;
 
-  // $scope.total_votes = totalVotes;
-  Vote.total().then(function(something){
-    $scope.total_votes = something;
-    console.log(something);
-  });
-  // console.log(totalVotes);
+  $scope.pt_percentage = $filter('number')($scope.total_votes.pt);
+  // (100*total_votes.none/total_votes.total)| number:2
+
 });
